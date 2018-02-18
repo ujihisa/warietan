e1b108d9dcf1958e4b367196399172f3d7cf593e
ujihisa <ujihisa@gmail.com>
2018-02-18 17:24:11 +0900

    Respond as small as possible

--- app/controllers/slack_events_controller.rb
+++ app/controllers/slack_events_controller.rb
@@ -3,8 +3,10 @@ class SlackEventsController < ApplicationController
   def index
     # mention:
     # {"token"=>"aFIwScpfjjm7O6KRAXU7rH6h", "team_id"=>"T8YE2DRT3", "api_app_id"=>"A99Q14KPG", "event"=>{"type"=>"message", "user"=>"U98PNCASE", "text"=>"test", "ts"=>"1518935809.000007", "channel"=>"C992P87ED", "event_ts"=>"1518935809.000007"}, "type"=>"event_callback", "event_id"=>"Ev9A72H3PA", "event_time"=>1518935809, "authed_users"=>["U98PNCASE"], "slack_event"=>{"token"=>"aFIwScpfjjm7O6KRAXU7rH6h", "team_id"=>"T8YE2DRT3", "api_app_id"=>"A99Q14KPG", "event"=>{"type"=>"message", "user"=>"U98PNCASE", "text"=>"test", "ts"=>"1518935809.000007", "channel"=>"C992P87ED", "event_ts"=>"1518935809.000007"}, "type"=>"event_callback", "event_id"=>"Ev9A72H3PA", "event_time"=>1518935809, "authed_users"=>["U98PNCASE"]}}
-    if params.dig('event', 'username') == 'warietan'
-      render json: params
+    if params.dig('type') == 'url_verification'
+      render json: {challenge: params.dig('challenge')}
+    elsif params.dig('event', 'username') == 'warietan'
+      head :ok
     elsif params.dig('event', 'type') == 'message'
       text = params.dig('event', 'text')
       channel = params.dig('event', 'channel')
@@ -14,9 +16,9 @@ class SlackEventsController < ApplicationController
         text: text,
       }
       system('curl', '-sif', '-d', URI.encode_www_form(post_params), 'https://slack.com/api/chat.postMessage')
-      render json: params
+      head :ok
     else
-      render json: params
+      head :ok
     end
   end
 end
